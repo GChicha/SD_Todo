aab1c659163ae1c61a184dfb9d8a29cfac5fc222
Convidado <guilherme.chichanoski@gmail.com>
2018-04-04 08:59:36 -0300

    coloquei os trolss

--- Servidor.java
+++ Servidor.java
@@ -1,8 +1,8 @@
 import java.rmi.RemoteException;
 import java.util.List;
 
-public interface Servidor {
-	public void addTask(Task task);
-	public void doneTask(int id);
-	public List<Task> listTask();
+public interface Servidor extends java.rmi.Remote {
+	public void addTask(Task task) throws RemoteException;
+	public void doneTask(int id) throws RemoteException;
+	public List<Task> listTask() throws RemoteException;
 }
