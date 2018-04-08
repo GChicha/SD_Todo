8cf55c4baf721d4277307704e7e0f2cc98382999
Guilherme Chichanoski <guilherme.chichanoski@gmail.com>
2018-04-08 18:36:45 -0300

    add cliente

---
+++ ClienteMain.java
@@ -0,0 +1,35 @@
+import java.rmi.Naming;
+import java.util.*;
+
+public class ClienteMain {
+	public static void main(String args[]) {
+		try {
+			Servidor s = (Servidor) Naming.lookup("//localhost:1099/Todo");
+
+			if (args.length < 1) {
+				System.out.println(s.listTask());
+			} else if (args.length > 1){
+				if (args[0].equals("add")) {
+					String taskDesc = "";
+					for (int i = 1; i < args.length; i++) {
+						System.out.println(args[i]);
+						taskDesc += args[i];
+					}
+					System.out.println(taskDesc);
+
+					Task task = new Task(taskDesc);
+
+					s.addTask(task);
+				} else if (args[0].equals("do")) {
+					s.doneTask(Integer.parseInt(args[1]));
+				} else {
+					throw new Exception("Errado");
+				}
+			} else {
+				throw new Exception("Errado");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
